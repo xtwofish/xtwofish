c6dc5f8ead5c7e574a6644757b6bf6700923230c
陳欣妤 <s3a432058@students.ncut.edu.tw>
2017-12-22 21:28:02 +0800

    修改apply、applymanager、home樣板(刪掉上面那條)

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -4,18 +4,7 @@
 
 @section('content')
 
-    <section id="inner-headline">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <ul class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-                        <li class="active">Portfolio</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
+
     <section id="content">
         <div class="container">
             <div class="row">
