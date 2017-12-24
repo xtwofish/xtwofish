c09421daa98b8de489782e73de7cbb5781950c4d
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-12-24 15:58:37 +0800

    申請表單 (2)

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -16,30 +16,65 @@
                     <div id="errormessage"></div>
                     <form action="" method="post" role="form" class="contactForm">
                         {{--日期--}}
-                        <div class="form-group">
-                            <input type="date" name="name" class="form-control" id="name" />
-                            <div class="validation"></div>
+                        <div class="col-xs-4 col-sm-2 col-md-2">
+                            <div class="form-group">
+                              <label>日期</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-20 col-sm-10 col-md-10">
+                            <div class="form-group">
+                                <input type="date" name="name" class="form-control" id="name"  value="<?php echo date("Y-m-d");?>">
+                            </div>
                         </div>
                         {{--教室--}}
-                        <div class="form-group">
-                            <select name="YourClassroom" class="form-control" >
-                                　<option>申請教室</option>
-                                　<option value="M501">M501</option>
-                                　<option value="M502">M502</option>
-                                <option value="M503">M503</option>
-                                <option value="M504">M504</option>
-                                <option value="M509">M509</option>
-                                <option value="M510">M510</option>
-                                <option value="M511">M511</option>
-                                <option value="M513">M512</option>
-                                <option value="M514">M514</option>
-                                <option value="M516">M516</option>
-                            </select>
-                            <div class="validation"></div>
+                        <div class="col-xs-4 col-sm-2 col-md-2">
+                            <div class="form-group">
+                                <label>申請教室</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-20 col-sm-10 col-md-10">
+                            <div class="form-group">
+                                <select name="YourClassroom" class="form-control" >
+                                    　<option>申請教室</option>
+                                    　<option value="M501">M501</option>
+                                    　<option value="M502">M502</option>
+                                    <option value="M503">M503</option>
+                                    <option value="M504">M504</option>
+                                    <option value="M509">M509</option>
+                                    <option value="M510">M510</option>
+                                    <option value="M511">M511</option>
+                                    <option value="M513">M512</option>
+                                    <option value="M514">M514</option>
+                                    <option value="M516">M516</option>
+                                </select>
+                            </div>
+                        </div>
+                        {{--開始時間--}}
+                        <div class="col-xs-4 col-sm-2 col-md-2">
+                            <div class="form-group">
+                                <label>開始時間</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-20 col-sm-10 col-md-10">
+                            <div class="form-group">
+                                <input type="time" name="starttime" class="form-control" id="starttime" value="08:00"/>
+                            </div>
+                        </div>
+                        {{--結束時間--}}
+                        <div class="col-xs-4 col-sm-2 col-md-2">
+                            <div class="form-group">
+                                <label>結束時間</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-20 col-sm-10 col-md-10">
+                            <div class="form-group">
+                                <input type="time" name="endtime" class="form-control" id="endtime" value="17:00"/>
+                            </div>
                         </div>
                         {{--班級--}}
+                        <div class="col-xs-24 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="班級" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                            <input type="text" name="name" class="form-control"  id="name" placeholder="班級" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                             <div class="validation"></div>
                         </div>
                         {{--姓名--}}
@@ -52,32 +87,11 @@
                             <input type="text" name="name" class="form-control" id="name" placeholder="手機號碼" data-rule="minlen:10" data-msg="Please enter at least 4 chars" />
                             <div class="validation"></div>
                         </div>
-                        {{--開始時間--}}
-                        <div class="form-group">
-                            <input type="time" name="name" class="form-control" id="name"  />
-                            <div class="validation"></div>
-                        </div>
-                        {{--結束時間--}}
-                        <div class="form-group">
-                            <input type="time" name="name" class="form-control" id="name"  />
-                            <div class="validation"></div>
-                        </div>
                         {{--申請事由--}}
                         <div class="form-group">
                             <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="申請事由"></textarea>
                             <div class="validation"></div>
                         </div>
-
-
-
-                        {{--<div class="form-group">--}}
-                            {{--<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />--}}
-                            {{--<div class="validation"></div>--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group">--}}
-                            {{--<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />--}}
-                            {{--<div class="validation"></div>--}}
-                        {{--</div>--}}
 
                         <div class="text-center"><button type="submit" class="btn btn-theme btn-block btn-md">送出</button></div>
                     </form>
