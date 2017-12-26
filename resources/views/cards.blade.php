173188426b27298ac9873a49015f128422912825
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-12-26 23:03:32 +0800

    修改後台3個頁面

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -8,176 +8,164 @@
       <hr>
       <!-- Icon Cards-->
       <div class="row">
-          {{--NO.1--}}
+          {{--501--}}
         <div class="col-xl-3 col-sm-6 mb-3">
           <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-comments"></i>
               </div>
               <div class="mr-5">501</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
               <span class="float-left">View Details</span>
               <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
             </a>
           </div>
         </div>
-          {{--NO.2--}}
+          {{--502--}}
         <div class="col-xl-3 col-sm-6 mb-3">
           <div class="card text-white bg-warning o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-list"></i>
               </div>
-              <div class="mr-5">11 New Tasks!</div>
+              <div class="mr-5">502</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
               <span class="float-left">View Details</span>
               <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
             </a>
           </div>
         </div>
-          {{--NO.3--}}
+          {{--503--}}
         <div class="col-xl-3 col-sm-6 mb-3">
           <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-shopping-cart"></i>
               </div>
-              <div class="mr-5">123 New Orders!</div>
+              <div class="mr-5">503</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
               <span class="float-left">View Details</span>
               <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
             </a>
           </div>
         </div>
-          {{--NO.4--}}
+          {{--504--}}
         <div class="col-xl-3 col-sm-6 mb-3">
           <div class="card text-white bg-danger o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-support"></i>
+                {{--<i class="fa fa-fw fa-support"></i>--}}
               </div>
-              <div class="mr-5">13 New Tickets!</div>
+              <div class="mr-5">504</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
               <span class="float-left">View Details</span>
               <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
             </a>
           </div>
         </div>
-          {{--NO.5--}}
+          {{--509--}}
           <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card text-white bg-dark o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">509</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
-          {{--NO.6--}}
+          {{--510--}}
           <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card text-white bg-secondary o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">510</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
-          {{--NO.7--}}
+          {{--511--}}
           <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card text-white bg-info o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">511</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
-          {{--NO.8--}}
+          {{--513--}}
           <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card text-white bg-warning o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">513</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
-          {{--NO.9--}}
+          {{--514--}}
           <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card text-white bg-success o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">514</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
-          {{--NO.10--}}
+          {{--516--}}
           <div class="col-xl-3 col-sm-6 mb-3">
               <div class="card text-white bg-danger o-hidden h-100">
                   <div class="card-body">
                       <div class="card-body-icon">
-                          <i class="fa fa-fw fa-support"></i>
+                          {{--<i class="fa fa-fw fa-support"></i>--}}
                       </div>
-                      <div class="mr-5">13 New Tickets!</div>
+                      <div class="mr-5">516</div>
                   </div>
                   <a class="card-footer text-white clearfix small z-1" href="#">
                       <span class="float-left">View Details</span>
                       <span class="float-right">
-                <i class="fa fa-angle-right"></i>
               </span>
                   </a>
               </div>
           </div>
+
 
       </div>
     </div>
