877969b266d1db0ffa00c0880fbc13a73f599ace
陳欣妤 <s3a432058@students.ncut.edu.tw>
2017-12-27 18:24:28 +0800

    設定Model到對應的資料表及欄位

--- app/Apply.php
+++ app/Apply.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apply extends Model
 {
-    //
+    //設定Model到對應的資料表
+    protected $table = 'applys';
+
+    protected $fillable = [
+        'class_id',
+        'user_class',
+        'user_name',
+        'user_phone',
+        'req_date',
+        'req_start',
+        'req_end',
+        'req_reason',
+    ];
 }
