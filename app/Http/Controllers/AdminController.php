a807d5495ba392063f357a17164a52e85d66e795
陳欣妤 <s3A432058@student.ncut.edu.tw>
2017-12-28 23:31:15 +0800

    利用show的方法,將applys內資料表內的所有資料呈現在後端

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Apply;
 
 class AdminController extends Controller
 {
     public function admin()
     {
         return view('admin');
+    }
+
+    public function Show(){
+        $applys=Apply::all();
+        return view('admin',compact('applys'));
     }
 }
