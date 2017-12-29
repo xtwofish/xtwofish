8108aa85e8276748cce5ac6e0db784bc7b54f6b6
陳欣妤 <s3a432058@students.ncut.edu.tw>
2017-12-29 17:28:33 +0800

    刪除table的view及controller

--- app/Http/Controllers/TablesController.php
+++ app/Http/Controllers/TablesController.php
@@ -1,13 +1,13 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class TablesController extends Controller
-{
-    public function tables()
-    {
-        return view('tables');
-    }
-}
+//
+//namespace App\Http\Controllers;
+//
+//use Illuminate\Http\Request;
+//
+//class TablesController extends Controller
+//{
+//    public function tables()
+//    {
+//        return view('tables');
+//    }
+//}
--- resources/views/tables.blade.php
+++ resources/views/tables.blade.php
@@ -1,27 +1,27 @@
-@extends('layouts.admin-master')
+{{--@extends('layouts.admin-master')--}}
 
-@section('title', 'Clean Blog')
+{{--@section('title', 'Clean Blog')--}}
 
-@section('content')
-    <!-- Breadcrumbs-->
-    <!-- Example DataTables Card-->
-    <div class="card mb-3">
-        <div class="card-header">
-            <i class="fa fa-table"></i>  歷史紀錄</div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th>日期</th>
-                        <th>教室</th>
-                        <th>開始時間</th>
-                        <th>結束時間</th>
-                        <th>班級</th>
-                        <th>姓名</th>
-                        <th>申請事由</th>
-                    </tr>
-                    </thead>
+{{--@section('content')--}}
+    {{--<!-- Breadcrumbs-->--}}
+    {{--<!-- Example DataTables Card-->--}}
+    {{--<div class="card mb-3">--}}
+        {{--<div class="card-header">--}}
+            {{--<i class="fa fa-table"></i>  歷史紀錄</div>--}}
+        {{--<div class="card-body">--}}
+            {{--<div class="table-responsive">--}}
+                {{--<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">--}}
+                    {{--<thead>--}}
+                    {{--<tr>--}}
+                        {{--<th>日期</th>--}}
+                        {{--<th>教室</th>--}}
+                        {{--<th>開始時間</th>--}}
+                        {{--<th>結束時間</th>--}}
+                        {{--<th>班級</th>--}}
+                        {{--<th>姓名</th>--}}
+                        {{--<th>申請事由</th>--}}
+                    {{--</tr>--}}
+                    {{--</thead>--}}
                     {{--<tfoot>--}}
                     {{--<tr>--}}
                     {{--<th>日期</th>--}}
@@ -33,68 +33,68 @@
                     {{--<th>申請事由</th>--}}
                     {{--</tr>--}}
                     {{--</tfoot>--}}
-                    <tbody>
-                    <tr>
-                        <td>2017-05-06</td>
-                        <td>M501</td>
-                        <td>上午8:00</td>
-                        <td>下午5:00</td>
-                        <td>四資二乙</td>
-                        <td>丁小胖</td>
-                        <td>我就是想要借阿，你管我，我還不要還呢 ! </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-    </div>
-    </div>
-    <!-- /.container-fluid-->
-    <!-- /.content-wrapper-->
-    <footer class="sticky-footer">
-        <div class="container">
-            <div class="text-center">
-                <small>Copyright © Your Website 2017</small>
-            </div>
-        </div>
-    </footer>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="loginmanager.blade.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Page level plugin JavaScript-->
-    <script src="vendor/datatables/jquery.dataTables.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin.min.js"></script>
-    <!-- Custom scripts for this page-->
-    <script src="js/sb-admin-datatables.min.js"></script>
-    </div>
-    </body>
+                    {{--<tbody>--}}
+                    {{--<tr>--}}
+                        {{--<td>2017-05-06</td>--}}
+                        {{--<td>M501</td>--}}
+                        {{--<td>上午8:00</td>--}}
+                        {{--<td>下午5:00</td>--}}
+                        {{--<td>四資二乙</td>--}}
+                        {{--<td>丁小胖</td>--}}
+                        {{--<td>我就是想要借阿，你管我，我還不要還呢 ! </td>--}}
+                    {{--</tr>--}}
+                    {{--</tbody>--}}
+                {{--</table>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--<!-- /.container-fluid-->--}}
+    {{--<!-- /.content-wrapper-->--}}
+    {{--<footer class="sticky-footer">--}}
+        {{--<div class="container">--}}
+            {{--<div class="text-center">--}}
+                {{--<small>Copyright © Your Website 2017</small>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</footer>--}}
+    {{--<!-- Scroll to Top Button-->--}}
+    {{--<a class="scroll-to-top rounded" href="#page-top">--}}
+        {{--<i class="fa fa-angle-up"></i>--}}
+    {{--</a>--}}
+    {{--<!-- Logout Modal-->--}}
+    {{--<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
+        {{--<div class="modal-dialog" role="document">--}}
+            {{--<div class="modal-content">--}}
+                {{--<div class="modal-header">--}}
+                    {{--<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>--}}
+                    {{--<button class="close" type="button" data-dismiss="modal" aria-label="Close">--}}
+                        {{--<span aria-hidden="true">×</span>--}}
+                    {{--</button>--}}
+                {{--</div>--}}
+                {{--<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>--}}
+                {{--<div class="modal-footer">--}}
+                    {{--<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>--}}
+                    {{--<a class="btn btn-primary" href="loginmanager.blade.php">Logout</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<!-- Bootstrap core JavaScript-->--}}
+    {{--<script src="vendor/jquery/jquery.min.js"></script>--}}
+    {{--<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>--}}
+    {{--<!-- Core plugin JavaScript-->--}}
+    {{--<script src="vendor/jquery-easing/jquery.easing.min.js"></script>--}}
+    {{--<!-- Page level plugin JavaScript-->--}}
+    {{--<script src="vendor/datatables/jquery.dataTables.js"></script>--}}
+    {{--<script src="vendor/datatables/dataTables.bootstrap4.js"></script>--}}
+    {{--<!-- Custom scripts for all pages-->--}}
+    {{--<script src="js/sb-admin.min.js"></script>--}}
+    {{--<!-- Custom scripts for this page-->--}}
+    {{--<script src="js/sb-admin-datatables.min.js"></script>--}}
+    {{--</div>--}}
+    {{--</body>--}}
 
-    </html>
-@endsection
+    {{--</html>--}}
+{{--@endsection--}}
