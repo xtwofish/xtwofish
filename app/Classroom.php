e8622c78f3e95a9a98b735fe0084e44a97d2b76d
陳美妤 <s3a432090@student.ncut.edu.tw>
2018-04-19 12:06:23 +0800

    新增Classroom資料表跟Model

---
+++ app/Classroom.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+    //設定Model到對應的資料表
+    protected $table = 'classrooms';
+
+    protected $fillable = [
+        'name',
+        'type',
+        'photo',
+        'status'
+    ];
+}
